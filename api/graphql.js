daec778084b2e9372303add2864c33451635cbf4
bikutoria <victoria.paskannaya@gmail.com>
2024-02-01 22:45:53 -0500

    debugging endlessly

--- api/graphql.js
+++ api/graphql.js
@@ -16,8 +16,8 @@ export default async function handler(req, res) {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', 'https://studio.apollographql.com');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Headers');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD');
 
   if (req.method === 'OPTIONS') {
     // Respond to OPTIONS request for preflight with status 200
