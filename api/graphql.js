9c8bec24f57390c4a5b1e1f314d24d04020e4b55
bikutoria <victoria.paskannaya@gmail.com>
2024-02-01 22:56:51 -0500

    ugh

--- api/graphql.js
+++ api/graphql.js
@@ -15,7 +15,7 @@ const startServer = apolloServer.start();
 export default async function handler(req, res) {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Origin', 'https://studio.apollographql.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://studio.apollographql.com, *');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Headers');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD');
 
