7578e4cf8bdd69bebde86c11afb1e5d81b6e2eb8
bikutoria <victoria.paskannaya@gmail.com>
2024-02-01 22:07:08 -0500

    edited paths and added introspection

--- api/graphql.js
+++ api/graphql.js
@@ -1,17 +1,37 @@
-const { ApolloServer } = require('apollo-server-micro');
-const typeDefs = require('.schema.js'); // Adjust path as needed
-const resolvers = require('.resolvers.js'); 
+import { ApolloServer, gql } from 'apollo-server-micro';
+// Import your schema and resolvers as before
+const typeDefs = require('../schema.js');
+const resolvers = require('../resolvers.js'); 
 
 const apolloServer = new ApolloServer({
-    typeDefs,
-    resolvers,
-    introspection: true,
-  });
-  
-  export const config = {
-    api: {
-      bodyParser: false,
-    },
-  };
-  
-  export default apolloServer.createHandler({ path: "/api/graphql" });
+  typeDefs,
+  resolvers,
+  // other options
+});
+
+const startServer = apolloServer.start();
+
+export default async function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.end();
+    return false;
+  }
+
+  await startServer;
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res);
+}
+
+// This disables the default body parser to improve performance
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
