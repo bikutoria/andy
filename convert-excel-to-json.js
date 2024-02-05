4233369127bfbd51068e83023762ed826086a6ea
bikutoria <victoria.paskannaya@gmail.com>
2024-02-04 20:55:33 -0500

    debugging saga

--- convert-excel-to-json.js
+++ convert-excel-to-json.js
@@ -1,5 +1,4 @@
-//Execute this script with Node.js whenever you need to update the JSON data from your Excel files:
-//node convert-excel-to-json.js
+//or run node convert-excel-to-json.js
 
 const XLSX = require('xlsx');
 const fs = require('fs');
@@ -13,6 +12,11 @@ function convertExcelToJson(filePath) {
     const sheetName = workbook.SheetNames[0]; // Or specify your sheet name
     const worksheet = workbook.Sheets[sheetName];
     return XLSX.utils.sheet_to_json(worksheet);
+
+    // Ensure data is always an array
+    if (!Array.isArray(data)) {
+        data = [data];
+    }
 }
 
 // Convert and save the data as JSON
@@ -20,3 +24,5 @@ const jsonData = convertExcelToJson(excelFilePath);
 fs.writeFileSync('data.json', JSON.stringify(jsonData, null, 2), 'utf8');
 
 console.log('Excel file has been converted to JSON');
+
+///
--- resolvers.js
+++ resolvers.js
@@ -3,15 +3,6 @@ const data = require('./data.json'); // Assuming your JSON data is an array
 const resolvers = {
   Query: {
     questionItems: () => data,
-    //v1
-    //questionItemByID: (_, { id }) => data.find(item => item.ID === Number(id)),
-    //v2
-    questionItemByID: (_, { id }) => {
-      console.log("Requested ID:", id); // Log the requested ID
-      console.log("Available data:", data); // Log the data to inspect its structure
-      const item = data.find(item => item.ID === parseInt(id, 10));
-      console.log("Found item:", item); // Log the found item, if any
-      return item;
-    }
+    questionItemByID: (_, { id }) => data.find(item => item.ID === Number(id)),
   },
 };
